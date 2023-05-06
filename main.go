5f351b4a19d10d7212678e49fbb5038a1be91623
Giovanni Prisco <gprisco01@gmail.com>
2023-05-06 18:35:47 +0200

    Using new ReceiveSearchResponse

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net"
 
 	"github.com/Gprisco/yeelightcontrol/yeelight"
@@ -16,23 +16,24 @@ func main() {
 	// Step 1: Create a UDP connection and bind to a local address.
 	conn, err := net.ListenPacket("udp", localAddr)
 	if err != nil {
-		fmt.Printf("Failed to bind to local address: %v\n", err)
+		log.Printf("Failed to bind to local address: %v\n", err)
 		return
 	}
 	defer conn.Close()
 
 	// Step 2: Multicast the search request message.
-	yeelight.SendSearchMessage(conn, multicastAddr)
+	if err = yeelight.SendSearchMessage(conn, multicastAddr); err != nil {
+		log.Printf("Failed to broadcast search message: %v\n", err)
+		return
+	}
 
 	// Step 3: Wait for the response message.
-	buf := make([]byte, 2048)
-	for {
-		n, addr, err := conn.ReadFrom(buf)
+	response, err := yeelight.ReceiveSearchResponse(conn)
 
-		if err != nil {
-			fmt.Printf("Failed to receive message: %v\n", err)
-			return
-		}
-		fmt.Printf("Received message from %s: %s\n", addr.String(), string(buf[:n]))
+	if err != nil {
+		log.Printf("Failed to get response from yeelight: %v\n", err)
+		return
 	}
+
+	log.Println(response)
 }
